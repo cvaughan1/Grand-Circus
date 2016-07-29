79348ad4b60d9019f7c749e99fbe5699b66df74c
Clark Vaughan <clarkvaughan111@yahoo.com>
2016-07-29 15:08:20 -0400

    Late update

--- NewProject/src/Labs/Lab4Factorial.java
+++ NewProject/src/Labs/Lab4Factorial.java
@@ -1,10 +1,23 @@
 package Labs;
 
+import java.util.Scanner;
+
 public class Lab4Factorial {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+		
+		System.out.println("Please enter a number fron 1 to 10");
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("here: ");
+		long num = sc.nextLong();
+	
+		
+	int result = 1;
+			       for (int i = 1; i <= num; i++) {
+			           result = result * i;
+			       }
+			       System.out.println("The factorial of " + num + " is " + result);
+			   }
+			}
+		
