9eaafcbf68c98f985bd644f31cfa4551b3662947
Clark Vaughan <clarkvaughan111@yahoo.com>
2016-08-01 11:03:08 -0400

    Re-do of Lab 3

--- NewProject/src/Labs/Lab3Powers.java
+++ NewProject/src/Labs/Lab3Powers.java
@@ -1,20 +1,43 @@
 package Labs;
+import java.util.Scanner;
 
 public class Lab3Powers {
 
-	public static void main(String[] args) {
-		while (choice.equalsIgnoreCase("y") {
+		public static void main(String[] args) {
 			
-			System.out.print("Enter an integer: ");
-			int num = Integer.parseInt(sc.nextLine());
-			
-			
-			
-			// 
-			for (int i = 1; i <= num; i = i + 1);
-				
-		}
-
+	        System.out.println("Welcome to the Squares and Cubes Table");
+	        System.out.println();
+	         
+	        //Scanner object
+	        Scanner sc = new Scanner(System.in);
+	         
+	         
+	         
+	        String choice = "y";
+	        while(choice.equalsIgnoreCase("y"))
+	        {
+	                // input from the user
+	                System.out.println("Enter an Integer: ");
+	                int integerNext = sc.nextInt();     
+	                 
+	                
+	                System.out.println("Number" + "  " + "Squared" + "  " + "Cubed");       
+	                System.out.println("======" + "  " + "======");
+			  for (int i = 1; i <= integerNext; i++)
+		        {      
+		        int numberSquared = (int) Math.pow(i, 2);
+		        int numberCubed = (int) Math.pow (i, 3); 
+		        String message = i + "\t" + numberSquared + "\t" + numberCubed;
+		         
+		        System.out.println(message);
+		        System.out.println();
+		        }
+		        //Continue?
+		        System.out.print("Continue? (y/n): ");
+		        choice = sc.next();
+		        System.out.println();
+		        }
+	        }
 	}
 
-}
+
