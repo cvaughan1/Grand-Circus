f4f1c5c85ce83c68d61ad98780173deeacfcd3e8
Clark Vaughan <clarkvaughan111@yahoo.com>
2016-08-01 11:08:52 -0400

    Latest Changes

--- NewProject/src/Labs/Lab3Powers.java
+++ NewProject/src/Labs/Lab3Powers.java
@@ -5,14 +5,12 @@ public class Lab3Powers {
 
 		public static void main(String[] args) {
 			
-	        System.out.println("Welcome to the Squares and Cubes Table");
+	        System.out.println("Learn your squares and cubes");
 	        System.out.println();
-	         
-	        //Scanner object
+	       
 	        Scanner sc = new Scanner(System.in);
-	         
-	         
-	         
+	      
+       	         
 	        String choice = "y";
 	        while(choice.equalsIgnoreCase("y"))
 	        {
@@ -22,7 +20,7 @@ public class Lab3Powers {
 	                 
 	                
 	                System.out.println("Number" + "  " + "Squared" + "  " + "Cubed");       
-	                System.out.println("======" + "  " + "======");
+	                System.out.println("======" + "  " + "======"+ "  " + "======");
 			  for (int i = 1; i <= integerNext; i++)
 		        {      
 		        int numberSquared = (int) Math.pow(i, 2);
@@ -36,6 +34,7 @@ public class Lab3Powers {
 		        System.out.print("Continue? (y/n): ");
 		        choice = sc.next();
 		        System.out.println();
+		        sc.close();
 		        }
 	        }
 	}
